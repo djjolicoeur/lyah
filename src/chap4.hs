c110eecf1b1f2dd24db13da345de2408411d98cf
Daniel Jolicoeur <djjolicoeur@gmail.com>
2018-07-31 23:19:53 -0400

    rest of chap4

--- src/chap4.hs
+++ src/chap4.hs
@@ -11,3 +11,49 @@ replicate' :: Int -> a -> [a]
 replicate' n x
   | n <= 0 = []
   | otherwise = x : replicate' (n-1) x
+
+-- Fake Take
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n - 1) xs
+
+-- Fake reverse
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+-- repeat.  Careful, does exactly what it says.  use w/ take'
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+-- zip, combine two lists into single list of pairs 
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+-- elem (contains? like)
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+  | a == x = True
+  | otherwise = elem' a xs
+
+-- Quicksort
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerOrEqual = [a | a <- xs, a <= x]
+      larger = [a | a <- xs, a > x]
+  in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+  
+
