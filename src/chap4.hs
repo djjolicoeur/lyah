2b57412268cc4231c90a358c899a64f5403ccda0
djjolicoeur <djjolicoeur@gmail.com>
2018-07-31 22:24:14 -0400

    chap4

---
+++ src/chap4.hs
@@ -0,0 +1,13 @@
+-- Chap 4, recursion
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list!"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+-- Clone of replicate
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n-1) x
