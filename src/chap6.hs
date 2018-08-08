491f03fa98d01f8f6d79b8846a75771b45ae3e97
Daniel Jolicoeur <djjolicoeur@gmail.com>
2018-08-07 22:43:20 -0400

    Okay, maybe one more page

--- src/chap6.hs
+++ src/chap6.hs
@@ -11,6 +11,7 @@
 -- just like clojure (:require [clojure.string :as str])
 
 import Data.List as L
+import Data.Char as C
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . L.nub
@@ -18,5 +19,21 @@ numUniques = length . L.nub
 wordNums :: String -> [(String, Int)]
 wordNums = map (\ws -> (head ws, length ws)) . L.group . sort . L.words
 
+-- isIn: is needle a sublist of haystack?
+
 isIn :: (Eq a) => [a] -> [a] -> Bool
-needle `isIn` haystack = L.any (needle `L.isPrefixOf`) (L.tails haystack)
+isIn needle haystack = L.any (needle `L.isPrefixOf`) (L.tails haystack)
+
+
+-- Caeser cipher
+
+encode :: Int -> String -> String
+encode offset msg = map (\c -> C.chr $ C.ord c + offset) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
+
+
+
+
